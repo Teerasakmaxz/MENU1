8c563de0730defa0d0eb0301a5d0aad48971ba01
Teerasak <maxz_drum@icloud.com>
2016-08-25 11:52:21 +0700

    วันแรกเส็จแล้ว

---
+++ app/src/main/java/com/example/maxz/menu1/Main2Activity.java
@@ -0,0 +1,28 @@
+package com.example.maxz.menu1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ThemedSpinnerAdapter;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+
+
+    }//main method
+}//main class
